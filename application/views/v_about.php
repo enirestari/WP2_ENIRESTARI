ded84c516959f881f1a45f4eb783d298974bf9cb
enirestari <enirestariii@gmail.com>
2022-10-01 16:50:41 +0700

    tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1> 
+     <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Eni </li>
+        <li>Nama Belakang : restari</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Tanjung Raya 2</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>mungguk keladan</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>tenis meja</li>
+        <li>Catur</li>
+    </ul>
+</section> 
